bed961503c50aa8273360a45d8bc48fd51af5d22
Yeasinoncode98 <devoncode98@gmail.com>
2025-11-02 03:15:31 +0600

    Added Comment for Crud -server

--- Simple-Crud-Server/index.js
+++ Simple-Crud-Server/index.js
@@ -105,7 +105,7 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Simple Crud Server is Running on Port ${port}`);
+  console.log(`Simple Crud Server is Running on this  Port named ${port}`);
 });
 
 /**
