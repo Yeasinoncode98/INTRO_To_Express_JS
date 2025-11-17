0f13cc96908c94a93f1e2240da91175268302cab
Yeasinoncode98 <devoncode98@gmail.com>
2025-11-18 02:39:43 +0600

    Added Client and Server

--- zap-shift-client/src/routes/router.jsx
+++ zap-shift-client/src/routes/router.jsx
@@ -7,6 +7,9 @@ import Login from "../assets/pages/Auth/login/Login";
 import Register from "../assets/pages/Auth/Register/Register";
 import Rider from "../assets/pages/Rider/Rider";
 import PrivateRoute from "./PrivateRoute";
+import SendParcel from "../assets/pages/SendParcel/SendParcel";
+import DashBoardLayout from "../Layouts/DashBoardLayout";
+import MyParcels from "../assets/pages/Dashboard/MyParcels/MyParcels";
 
 export const router = createBrowserRouter([
   {
@@ -30,6 +33,15 @@ export const router = createBrowserRouter([
           </PrivateRoute>
         ),
       },
+      {
+        path: "/send-parcel",
+        element: (
+          <PrivateRoute>
+            <SendParcel></SendParcel>
+          </PrivateRoute>
+        ),
+        loader: () => fetch("/serviceCenter.json").then((res) => res.json()),
+      },
     ],
   },
   {
@@ -43,6 +55,20 @@ export const router = createBrowserRouter([
       {
         path: "/register",
         Component: Register,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashBoardLayout></DashBoardLayout>
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        path: "my-parcels",
+        Component: MyParcels,
       },
     ],
   },
---
+++ zap-shift-server/index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+// MongoDb
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Q2xv5A6rxYQ0USfY
+
+// MONGO URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pkimykw.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("zap_shift_db");
+    const parcelCollection = db.collection("parcels");
+
+    // Parcel api
+
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+
+      //   parcels?email = "",&
+      if (email) {
+        query.senderEmail = email;
+      }
+
+      const cursor = parcelCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      const result = await parcelCollection.insertOne(parcel);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connecti  on
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Zap shifting is live");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
